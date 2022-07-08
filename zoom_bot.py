d15d69801239d9cbc7cdf0817eee9036063783bd
Hassan <hassan.k.athumani@gmail.com>
2022-07-08 19:59:48 +0300

    minor changes

--- zoom_bot.py
+++ zoom_bot.py
@@ -4,20 +4,21 @@ import datetime
 import time
 import keyboard
 
+
 class zoom_bot:
-	def join(self,meeting_link):
-		self.bot = webdriver.Chrome("chromedriver.exe")
-		self.bot.get(meeting_link)
-		time.sleep(3)
-		keyboard.send("tab", do_press=True, do_release=True)
-		keyboard.send("tab", do_press=True, do_release=True)
-		keyboard.send("enter", do_press=True, do_release=True)
-		time.sleep(10)
+    def join(self, meeting_link):
+        self.bot = webdriver.Chrome("chromedriver.exe")
+        self.bot.get(meeting_link)
+        time.sleep(3)
+        keyboard.send("tab", do_press=True, do_release=True)
+        keyboard.send("tab", do_press=True, do_release=True)
+        keyboard.send("enter", do_press=True, do_release=True)
+        time.sleep(10)
 
-		self.bot.quit()
+        self.bot.quit()
 
-link = open("meeting_link.txt","r").read()
+
+link = open("meeting_link.txt", "r").read()
 
 obj = zoom_bot()
 obj.join(link)
-
--- zoom_bot_second.py
+++ zoom_bot_second.py
@@ -3,23 +3,26 @@ from selenium.webdriver.common.keys import Keys
 import datetime
 import time
 import keyboard
+
+
 class zoom_bot:
-	def join(self, meeting_link):
-		self.bot = webdriver.Chrome("chromedriver.exe")
-		self.bot.get(meeting_link)
-		time.sleep(5)
-		keyboard.send('tab', do_press=True, do_release=True)
-		keyboard.send('tab', do_press=True, do_release=True)
-		keyboard.send('enter', do_press=True, do_release=True)
-		time.sleep(5)
-		keyboard.send('tab', do_press=True, do_release=True)
-		keyboard.send('tab', do_press=True, do_release=True)
-		keyboard.send('tab', do_press=True, do_release=True)
-		keyboard.send('enter', do_press=True, do_release=True)
+    def join(self, meeting_link):
+        # self.bot = webdriver.Firefox(executable_path="geckodriver.exe")
+        self.bot = webdriver.Firefox(executable_path="geckodriver")
+        self.bot.get(meeting_link)
+        time.sleep(5)
+        keyboard.send('tab', do_press=True, do_release=True)
+        keyboard.send('tab', do_press=True, do_release=True)
+        keyboard.send('enter', do_press=True, do_release=True)
+        time.sleep(5)
+        keyboard.send('tab', do_press=True, do_release=True)
+        keyboard.send('tab', do_press=True, do_release=True)
+        keyboard.send('tab', do_press=True, do_release=True)
+        keyboard.send('enter', do_press=True, do_release=True)
 
-		self.bot.quit()
+        self.bot.quit()
 
 
-link = open("meeting_link.txt","r").read()
+link = open("meeting_link.txt", "r").read()
 obj = zoom_bot()
 obj.join(link)
